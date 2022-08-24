96ddba60ce3976acd381e4dbc962545253f9e36e
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2022-08-24 02:18:35 -0500

    Update pdf.hpp

--- pdf.hpp
+++ pdf.hpp
@@ -1,3 +1,16 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    Luca de Veintemilla
+				Kevin Vargas 
+                Cristopher Iza
+                Denisse Rea
+					
+	FECHA DE CREACION:        20-05-2022 	
+	FECHA DE MODIFICACION:    24-05-2022
+	Grupo #6
+*/
 #ifndef _PDF_HPP_
 #  define _PDF_HPP_
 
