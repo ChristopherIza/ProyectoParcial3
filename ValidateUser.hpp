f567fa7bbfae7ff036e75ef01bb7f270c9cbb93b
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2022-08-24 02:17:19 -0500

    Update ValidateUser.hpp

--- ValidateUser.hpp
+++ ValidateUser.hpp
@@ -1,15 +1,16 @@
-/**
- * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
- * @file ValidateUser.hpp
- * @author De Veintemilla Luca- Iza Christopher- Rea Denise - Vargas Kevin
- * @brief Class that manage operations of tree class
- * Github: https://github.com/ChristopherIza/Arboles_AVL.git
- * @version 0.1
- * @date 2022-07-19
- * Date last modification: 2022-07-14
- * @copyright Copyright (c) 2022
- * 
- */
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    Luca de Veintemilla
+				Kevin Vargas 
+                Cristopher Iza
+                Denisse Rea
+					
+	FECHA DE CREACION:        20-05-2022 	
+	FECHA DE MODIFICACION:    24-05-2022
+	Grupo #6
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
