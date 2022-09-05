d9e403d5f505e688f67bd695c7249c9694c22eaa
DeniseRea <dnrea@espe.edu.ec>
2022-09-04 19:08:53 -0500

    Update!

--- PDFManager.cpp
+++ PDFManager.cpp
@@ -5,7 +5,7 @@
 	AUTORES:    Luca de Veintemilla
 				Kevin Vargas 
                 Cristopher Iza
-                Denisse Rea
+                Denise Rea
 					
 	FECHA DE CREACION:        20-05-2022 	
 	FECHA DE MODIFICACION:    24-05-2022
